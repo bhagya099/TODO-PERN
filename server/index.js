fd057a0b51f778ff3725f0a0d3186ad92437ead2
Bhagyashree <bhagya099@gmail.com>
2022-09-13 11:45:42 +1000

    created a get and post routes

--- server/index.js
+++ server/index.js
@@ -17,16 +17,25 @@ app.post("/todos", async (req, res) => {
     // console.log(req.body);
     const { description } = req.body;
     const newTodo = await pool.query(
-      "INSERT INTO todo(description) VALUES($1);",
+      "INSERT INTO todo(description) VALUES($1) RETURNING *;",
       [description]
     );
+
+    res.json(newTodo);
   } catch (error) {
     console.log(error.message);
   }
 });
 
 //get all todos
-
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo;");
+    res.json(allTodos.rows);
+  } catch (error) {
+    console.log(error.message);
+  }
+});
 //get a todo
 
 //update todo
